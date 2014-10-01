bd6b1c68eeecd486b8c339916449028c8395aedb
Ryan Benson <benson.ryan@gmail.com>
2014-10-01 15:06:30 -0500

    Using raw API since the gem doesn't work and isn't supported anymore

--- archiveRepos.rb
+++ archiveRepos.rb
@@ -1,39 +1,42 @@
 #!/usr/bin/ruby
 
 #
-# requires the beanstalkapp gem! >> might need to be run as sudo
-# ~gem install beanstalkapp
+# requires the http and json gems
+# ~gem install http json
 # also requires that git is installed locally
 #
 
 require 'rubygems'
-require 'beanstalkapp'
+require 'net/http'
+require 'uri'
+require 'json'
 
-Beanstalk::API::Base.setup(
-	:domain		=> 'YOUR_SUBDOMAIN_HERE',
-	:login		=> 'YOUR_LOGIN',
-	:password	=> 'YOUR_PASSWORD'
-)
+uri = URI.parse('http://your_sub_domain.beanstalkapp.com/api/repositories.json')
 
-#collect our repos
-allRepos = Beanstalk::API::Repository.find(:all)
+http = Net::HTTP.new(uri.host, uri.port)
+request = Net::HTTP::Get.new(uri.request_uri)
+request.basic_auth("your_username", "your_auth_token")
+response = http.request(request)
+
+allRepos = JSON.parse(response.body)
+
+# #collect our repos
+# allRepos = Beanstalk::API::Repository.find(:all)
 
 allRepos.each do |repo|
-	repoId = repo.attributes['id']
-	repoData = Beanstalk::API::Repository.find(repoId)
-	repoUrl = repoData.attributes['repository_url']
-	repoName = repoData.attributes['name']
+	repoUrl = repo['repository']['repository_url']
+    repoName = repo['repository']['name']
 	
 	#need to see if we have the repo already or not. Can't re-clone if it already exists
 	if File.directory? repoName
 		#we already have the repo, just pull changes inside the right dir
-		cmd = "git pull"
+		cmd = "git remote update"
 		Dir.chdir(repoName) do
 			system cmd
 		end
 	else
 		#we don't have the repo yet, clone it
-		cmd = "git clone #{repoUrl}"
+		cmd = "git clone --mirror #{repoUrl}"
 		system cmd
 	end
 end
